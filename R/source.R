f840e6db60431796ba3538a30a19cb70179d63e1
Pawel Przytula <pawel@appsilon.pl>
2018-02-14 13:13:00 +0100

    Added skipping installation if package already exists and passes all requirements

--- R/source.R
+++ R/source.R
@@ -1,41 +1,81 @@
-stop <- function(text = "") {
-  message(paste("[error]", text, "\n"))
+log_print <- function(mode = "info", text = "", ...) {
+  cat(sprintf("[%s] %s\n", mode, sprintf(text, ...)))
+}
+
+log_check <- function(text = "", ...) {
+  log_print("check", text, ...)
+}
+
+log_info <- function(text = "", ...) {
+  log_print("info", text, ...)
+}
+
+log_error <- function(text = "", ...) {
+  log_print("error", text, ...)
+}
+
+stop_session <- function(text = "", ...) {
+  log_error(text, ...)
   quit(status = 1)
 }
 
+
+
 package_is_installed <- function(package, print_info = TRUE) {
   if (print_info) {
-    cat(paste("[check]", package, "package_is_installed", "\n"))
+    log_check("%s package_is_installed", package)
   }
   if (!is.element(package, installed.packages()[,1])) {
-    stop(paste("package", package, "is not installed!"))
+    stop_session("Package %s is not installed!", package)
   }
 }
 
 package_has_exact_version <- function(expected_version) {
   function(package) {
-    cat(paste("[check]", package, "package_has_exact_version", expected_version, "\n"))
+    log_check("%s package_has_exact_version %s", package, expected_version)
     package_is_installed(package, print_info = FALSE)
     current_version <- installed.packages()[package, "Version"]
     if (utils::compareVersion(current_version, expected_version) != 0) {
-      stop(paste0("Package `", package, "` version `", current_version, "` is not equal to `", expected_version, "`"))
+      stop_session("Package %s version %s is not equal to %s", package, current_version, expected_version)
     }
   }
 }
 
 package_has_minimum_version <- function(expected_minimum_version) {
   function(package) {
-    cat(paste("[check]", package, "package_has_minimum_version", expected_minimum_version, "\n"))
+    log_check("%s package_has_minimum_version %s", package, expected_minimum_version)
     package_is_installed(package, print_info = FALSE)
     current_version <- installed.packages()[package, "Version"]
     if (utils::compareVersion(current_version, expected_minimum_version) == -1) {
-      stop(paste0("Package `", package, "` version `", current_version, "` is not >= `", expected_minimum_version, "`"))
+      stop_session("Package %s version %s is not >= %s", package, current_version, expected_minimum_version)
     }
   }
 }
 
-install_and_verify <- function(install = install.packages, verify = package_is_installed, package = NULL, package_path = package, ...) {
-  if (is.null(package)) stop("Package name is null")
-  install(package_path, ...)
+install_and_verify <- function(install = install.packages, 
+                               verify = package_is_installed, 
+                               package = NULL, 
+                               package_path = package, 
+                               overwrite = FALSE, ...) {
+
+  if (is.null(package)) stop_session("Package name is null")
+
+  if (!overwrite) {
+    package_is_not_installed <- tryCatch({
+      for (v in c(verify)) v(package)
+      log_check("%s is already installed with provided requirements.", package)
+      FALSE
+    }, error = function(e) {
+      TRUE
+    })
+  }
+
+  if (package_is_not_installed || overwrite) {
+    log_info("Installing %s...", package)
+    install(package_path, ...) 
+  } else {
+    log_info("Skipping installation...")
+  }
+
   for (v in c(verify)) v(package)
 }
